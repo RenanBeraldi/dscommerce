f9ddad015a9c861975710c3bf2342c951a10228f
renan.beraldi <renanberaldi@outlook.com>
2025-03-24 21:09:44 -0300

    Implemented API layers

---
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscommerce.controllers;
+
+import com.devsuperior.dscommerce.dtos.ProductDTO;
+import com.devsuperior.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        ProductDTO productDTO = productService.findById(id);
+
+        return productDTO;
+    }
+}
